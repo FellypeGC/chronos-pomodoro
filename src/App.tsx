423a7db5ee434aaaea487bbae0bbd58eccb9f212
FellypeGC <gabrielfellypesteve@gmail.com>
2026-07-25 20:21:55 -0300

    import CSS Module file

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import Heading from "./components/Heading"
+
+
 import "./styles/theme.css"
 import "./styles/global.css"
 
@@ -5,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1>Hello, World!</h1>
+      <Heading />
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi voluptatibus architecto recusandae quam commodi est dolorum quasi quos accusantium nesciunt? Corrupti possimus ea facilis modi corporis voluptatem eum eaque beatae?</p>
     </>
   )
