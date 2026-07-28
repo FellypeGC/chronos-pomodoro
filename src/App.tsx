ae549f507ccd0f425092c64a91925de259c553a1
FellypeGC <gabrielfellypesteve@gmail.com>
2026-07-28 20:01:44 -0300

    add heading component + timer icon

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import Heading from "./components/Heading"
 
 import "./styles/theme.css"
 import "./styles/global.css"
+import { TimerIcon } from "lucide-react"
 
 function App() {
 
   return (
     <>
-      <Heading attr={123} attr2="String">
+      <Heading>
         Olá, Mundo!
+        <button>
+          <TimerIcon />
+        </button>
       </Heading>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi voluptatibus architecto recusandae quam commodi est dolorum quasi quos accusantium nesciunt? Corrupti possimus ea facilis modi corporis voluptatem eum eaque beatae?</p>
     </>
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,7 +1,11 @@
 import styles from "./Heading.module.css"
 
-const Heading = (props) => {
-  return <h1 className={styles.heading}>{props.children}</h1>
+type HeadingProps = {
+  children: React.ReactNode;
+}
+
+const Heading = ({ children }: HeadingProps) => {
+  return <h1 className={styles.heading}>{children}</h1>
 }
 
 export default Heading
