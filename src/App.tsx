019a2f1690154de12383099567ccbcf4a6781fdd
FellypeGC <gabrielfellypesteve@gmail.com>
2026-07-30 10:53:24 -0300

    add container component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Heading from "./components/Heading"
-
+import Container from "./components/Container"
 
 import "./styles/theme.css"
 import "./styles/global.css"
@@ -9,26 +9,15 @@ function App() {
 
   return (
     <>
-      <div className="container">
-        <div className="content">
-          <section>LOGO</section>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <section>MENU</section>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <section>FORMULÁRIO</section>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <section>FOOTER</section>
-        </div>
-      </div>
+      <Container>
+        <Heading>
+          Chronos Logo
+        </Heading>
+      </Container>
+
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
     </>
   )
 }
