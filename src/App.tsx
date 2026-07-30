cbc6645ac49f5205e0853315b60766328ac71744
FellypeGC <gabrielfellypesteve@gmail.com>
2026-07-30 15:55:15 -0300

    add logo component with icon and link

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import Heading from "./components/Heading"
 import Container from "./components/Container"
+import Logo from "./components/Logo"
 
 import "./styles/theme.css"
 import "./styles/global.css"
-import { TimerIcon } from "lucide-react"
 
 function App() {
 
   return (
     <>
       <Container>
-        <Heading>
-          Chronos Logo
-        </Heading>
+        <Logo />
       </Container>
 
       <Container>
---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,16 @@
+import { TimerIcon } from "lucide-react"
+import styles from "./styles.module.css"
+
+const Logo = () => {
+  return (
+    <div className={styles.logo}>
+      <a className={styles.logoLink} href="#">
+        <TimerIcon size={64} />
+        <span>Chronos</span>
+      </a>
+    </div>
+  )
+}
+
+export default Logo
+
