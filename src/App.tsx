0602c182149174a07b09b9675083887fdf6a7aee
FellypeGC <gabrielfellypesteve@gmail.com>
2026-08-06 23:31:48 -0300

    chore: add GenericHtml component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import Home from "./pages/Home"
+// import NotFound from "./pages/NotFound"
+// import AboutPomodoro from "./pages/AboutPomodoro"
 import "./styles/theme.css"
 import "./styles/global.css"
 
