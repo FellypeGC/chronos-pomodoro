82dc5f5eea2d25e96eddfdb900c595a18768f227
FellypeGC <gabrielfellypesteve@gmail.com>
2026-08-18 23:20:40 -0300

    feat: catch next cycle type (FizzBuzz algorithm)

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -6,12 +6,14 @@ import { useRef } from "react";
 import type { TaskModel } from "../../models/TaskModel";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
 
 const MainForm = () => {
   const { state, setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
 
   const nextCycle = getNextCycle(state.currentCycle);
+  const nextCycleType = getNextCycleType(nextCycle);
 
   function handleCreateNewTask(e: React.SubmitEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -32,7 +34,7 @@ const MainForm = () => {
       completeDate: null,
       interruptDate: null,
       duration: 1,
-      type: "workTime",
+      type: nextCycleType,
     };
 
     const secondsRemaining = newTask.duration * 60;
