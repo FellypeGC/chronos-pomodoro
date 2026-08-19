1a12c19007e5d264244e8276f261a9777f23f37d
FellypeGC <gabrielfellypesteve@gmail.com>
2026-08-19 16:46:49 -0300

    chore: format seconds in minutes

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -7,6 +7,7 @@ import type { TaskModel } from "../../models/TaskModel";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import { getNextCycle } from "../../utils/getNextCycle";
 import { getNextCycleType } from "../../utils/getNextCycleType";
+import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
 
 const MainForm = () => {
   const { state, setState } = useTaskContext();
@@ -48,7 +49,7 @@ const MainForm = () => {
         activeTask: newTask,
         currentCycle: nextCycle,
         secondsRemaining,
-        formattedSecondsRemaining: "00:00",
+        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
         tasks: [...prevState.tasks, newTask],
       };
     });
