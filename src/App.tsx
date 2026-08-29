6fde2574818a9f7f38d58c8f4857aeae2871ad19
FellypeGC <gabrielfellypesteve@gmail.com>
2026-08-29 00:13:30 -0300

    feat: create router and scrollToTop components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import Home from "./pages/Home";
+import MainRouter from "./routers/MainRouter";
 import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
 import MessagesContainer from "./components/MessagesContainer";
 
 import "./styles/theme.css";
 import "./styles/global.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import NotFound from "./pages/NotFound";
-import AboutPomodoro from "./pages/AboutPomodoro";
 
 function App() {
   return (
     <TaskContextProvider>
       <MessagesContainer>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about-pomodoro/" element={<AboutPomodoro />} />
-
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <MainRouter />
       </MessagesContainer>
     </TaskContextProvider>
   );
