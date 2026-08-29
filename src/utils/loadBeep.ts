0e4ebd2b564e737d87b0ea884b0a3e6b912c24c1
FellypeGC <gabrielfellypesteve@gmail.com>
2026-08-28 21:54:41 -0300

    feat: add play audio function for completed tasks

---
+++ src/utils/loadBeep.ts
@@ -0,0 +1,13 @@
+import gravitationalBeep from "../assets/audios/audios_gravitational_beep.mp3";
+
+export function loadBeep() {
+  const audio = new Audio(gravitationalBeep);
+  audio.load();
+
+  return () => {
+    audio.currentTime = 0;
+    audio
+      .play()
+      .catch((error) => console.log("Erro ao reproduzir o áudio", error));
+  };
+}
